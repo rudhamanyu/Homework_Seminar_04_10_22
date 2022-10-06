1d368833556d910fb5fac87fb86d26bbeac85ad5
RuslanGareev <rudhamanyu@gmail.com>
2022-10-06 18:59:55 +0500

    Second Digit

---
+++ Homework1/Program.cs
@@ -0,0 +1,14 @@
+﻿/*Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+void GetNumber(int number)
+{
+    int secondDigit = number % 100 / 10;
+    Console.WriteLine($"{number} -> {secondDigit}");
+}
+
+GetNumber(861);
