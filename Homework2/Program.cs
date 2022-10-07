bcb12f4d2cd0a1242d001b0f6538019b370170ba
RuslanGareev <rudhamanyu@gmail.com>
2022-10-07 12:13:46 +0500

    Get Third Digit

---
+++ Homework2/Program.cs
@@ -0,0 +1,20 @@
+﻿/*Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6
+*/
+
+void GetThirdDigit(int number)
+{
+    if (number > 99)
+    {
+        while (number > 999) 
+            number = number / 10;
+        int result = number % 10;
+        Console.WriteLine($"Третья цифра: {result}");
+    }
+    else
+        Console.WriteLine("Третьей цифры нет");
+}
+GetThirdDigit(12658);
